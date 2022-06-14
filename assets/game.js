acc8bab737723a4c63cea8116712296743ea3a48
J Burkholder <Jay.S.Burkholder@gmail.com>
2022-06-14 06:10:51 -0400

    another commit to mod 3 lessons

--- assets/game.js
+++ assets/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is freaking running!");
+var playerName = window.prompt("What is your robot's name?");
+
+// log player name
+console.log(playerName);
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+  fight();
